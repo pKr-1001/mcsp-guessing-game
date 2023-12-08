00aa6a9020c6860309305c16a9bace4b61e6a23a
park <minpark93@gmail.com>
2023-12-08 11:21:57 -0800

    feature 4,5 complete

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const maxGuess = 10;
 const numTarget = getRandomIntInclusive(minGuess, maxGuess);
 
 let count = 0;
+let nameInput = prompt("Enter your name")
+let arr = [];
+
 while (true) {
-    const result = guessOnce();
+    const result = guessOnce(nameInput);
     alert(result);
     count++;
     
     if( result === "correct")
     {
         console.log("Correct! It only took you " + count + " gusses!");
+        console.log("That's Correct " + nameInput + "! Your previous guesses were " + arr.join(','));
+
         break;
     }
 //     if(count === 2) {
@@ -23,9 +28,10 @@ while (true) {
 }
 // guessOnce();
 
-function guessOnce() {
-        let arr = [];
+function guessOnce(nameInput) {
+
         // use prompt to get user input
+        
         let input = prompt("Guess a number bewteen " + minGuess + " and " + maxGuess);
         console.log("The user gussed: " + input);
         // compare user guess to target
@@ -33,11 +39,11 @@ function guessOnce() {
         console.log("input as number " + inputNum);
         // inform user if guess is >, <, or = to target
         if(inputNum < numTarget) {
-            arr.push(input);
-            return "lower"
+             arr.push(input);
+            return "Sorry " + nameInput + ", Guess Higher"
         } else if (inputNum > numTarget) {
-            arr.push(input);
-            return "greater"
+             arr.push(input);
+            return "Sorry " + nameInput + ", Guess Lower"
         } else {
             // they are equal
             return "correct"
